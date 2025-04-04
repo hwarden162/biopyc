1bd813009fa47e87245a93fd2cb6473bb2cc2745
Hugh Warden <hugh.warden@outlook.com>
2025-04-04 16:40:23 +0100

    Add exclusion strings to create_image_schema

--- biopyc/schema/_create_image_schema.py
+++ biopyc/schema/_create_image_schema.py
@@ -1,7 +1,7 @@
 import os
 from glob import glob
 from pathlib import Path
-from typing import Callable, Optional
+from typing import Callable, List, Optional
 
 from pandas import DataFrame, concat
 
@@ -11,6 +11,7 @@ def create_image_schema(
     pattern: str = "*",
     recursive: bool = False,
     meta_func: Optional[Callable] = None,
+    exclusion_strings: List[str] = [],
 ) -> DataFrame:
     if not isinstance(input_folder, str):
         raise TypeError("Input folder should be a string")
@@ -22,8 +23,23 @@ def create_image_schema(
         raise TypeError("Recursive should be a boolean value")
     if meta_func is not None and not callable(meta_func):
         raise TypeError("Meta func should be callable")
+    if not isinstance(exclusion_strings, list):
+        raise TypeError("Exclusion strings should be a list")
+    if len(exclusion_strings) > 0:
+        if not all([isinstance(string, str) for string in exclusion_strings]):
+            raise TypeError("Every entry of exclusion strings should be a string")
     abs_input_pattern = os.path.join(str(Path(input_folder).resolve()), pattern)
     input_images = glob(abs_input_pattern, recursive=recursive)
+    if len(input_images) == 0:
+        raise FileNotFoundError("No input images found")
+    if len(exclusion_strings) > 0:
+        input_images = [
+            image
+            for image in input_images
+            if not any(excl in image for excl in exclusion_strings)
+        ]
+    if len(input_images) == 0:
+        raise ValueError("All files contain a string to exclude")
     if meta_func is None:
         return DataFrame({"Image_Intensity": input_images})
     dfs = []
