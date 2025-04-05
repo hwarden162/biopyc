9c2a81680f36e0b9a58e6a348e8a718f8aba335f
Hugh Warden <hugh.warden@outlook.com>
2025-04-05 16:10:20 +0100

    Adding tests

--- tests/schema/test_create_image_schema.py
+++ tests/schema/test_create_image_schema.py
@@ -1,4 +1,7 @@
 from biopyc.schema._create_image_schema import create_image_schema
+from glob import glob
+from pandas import concat, DataFrame
+from pathlib import Path
 from pytest import raises
 from tempfile import TemporaryDirectory
 
@@ -22,3 +25,41 @@ def test_create_image_schema_validation():
             create_image_schema(str(empty_dir))
     with raises(ValueError, match="All files contain a string to exclude"):
         create_image_schema("./tests/imgs", exclusion_strings=[".tif"])
+
+def test_create_image_schema():
+    # Test basic usage
+    image_file_paths = glob(str(Path("./tests/imgs/*.tif").resolve()))
+    reference_df = DataFrame({"Image_Intensity": image_file_paths})
+    test_df = create_image_schema("./tests/imgs", "*.tif")
+    assert test_df.equals(reference_df)
+    # Test Exclusion
+    image_file_paths = glob(str(Path("./tests/imgs/*.tif").resolve()))
+    image_file_paths = [image_file_path for image_file_path in image_file_paths if not "w5" in image_file_path]
+    reference_df = DataFrame({"Image_Intensity": image_file_paths})
+    test_df = create_image_schema("./tests/imgs", "*.tif", exclusion_strings=["w5"])
+    assert test_df.equals(reference_df)
+    
+    def image_meta_func(filename):
+        filename = Path(filename).name.split("_")
+        rename_dict = {"w1": "Image_Nuclei", "w2": "Image_ER", "w3": "Image_CytoRNA", "w4": "Image_AGP", "w5": "Image_Mito"}
+        return {
+            "Plate": filename[0],
+            "Row": filename[1][0],
+            "Column": filename[1][1:],
+            "Site": filename[2][1:],
+            "Stain": rename_dict[filename[3][:-4]]
+        }
+    
+    image_file_paths = glob(str(Path("./tests/imgs/*.tif").resolve()))
+    reference_dfs = []
+    for image in image_file_paths:
+        meta_dict = image_meta_func(image)
+        meta_dict = {
+            f"Metadata_{key}" if not key.startswith("Metadata_") else key: value
+            for key, value in meta_dict.items()
+        }
+        meta_dict["Image_Intensity"] = image
+        reference_dfs.append(DataFrame(meta_dict, index=[0]))
+    reference_df = concat(reference_dfs).reset_index(drop=True)
+    test_df = create_image_schema("./tests/imgs", "*.tif", meta_func=image_meta_func)
+    assert test_df.equals(reference_df)
