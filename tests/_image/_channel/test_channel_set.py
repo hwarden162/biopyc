f0f15bd718ce596099506fba7102e2eb98c1cce4
Hugh Warden <hugh.warden@outlook.com>
2025-04-05 23:31:33 +0100

    Add channel set tests

--- tests/_image/_channel/test_channel_set.py
+++ tests/_image/_channel/test_channel_set.py
@@ -32,11 +32,50 @@ def test_channel_set():
 def test_intensity_channel_set_validation():
     with raises(ValueError, match="Dtype of intensities should be numeric"):
         IntensityChannelSet(Channel(da.ones((5,5), dtype=bool), "Stain"))
+        
+def test_intensity_channel_set():
+    channels = [
+        Channel(da.ones((5,5), dtype=uint8), "Stain1"),
+        Channel(da.ones((5,5), dtype=uint8), "Stain2"),
+        Channel(da.ones((5,5), dtype=uint8), "Stain3"),
+        Channel(da.ones((5,5), dtype=uint8), "Stain4"),
+        Channel(da.ones((5,5), dtype=uint8), "Stain5")
+    ]
+    channel_set = IntensityChannelSet(channels)
+    assert channel_set.channels == channels
+    assert channel_set.shape == (5,5)
+    assert channel_set.dtype == uint8
 
 def test_label_mask_channel_set_validation():
     with raises(ValueError, match="Dtype of label masks should be an integer"):
         LabelMaskChannelSet(Channel(da.ones((5,5), dtype=float), "Stain"))
+
+def test_label_mask_channel_set():
+    channels = [
+        Channel(da.ones((5,5), dtype=uint8), "Stain1"),
+        Channel(da.ones((5,5), dtype=uint8), "Stain2"),
+        Channel(da.ones((5,5), dtype=uint8), "Stain3"),
+        Channel(da.ones((5,5), dtype=uint8), "Stain4"),
+        Channel(da.ones((5,5), dtype=uint8), "Stain5")
+    ]
+    channel_set = LabelMaskChannelSet(channels)
+    assert channel_set.channels == channels
+    assert channel_set.shape == (5,5)
+    assert channel_set.dtype == uint8
         
 def test_binary_mask_channel_set_validation():
     with raises(ValueError, match="Dtype of binary masks should be boolean"):
         BinaryMaskChannelSet(Channel(da.ones((5,5), dtype=int), "Stain"))
+
+def test_binary_mask_channel_set():
+    channels = [
+        Channel(da.ones((5,5), dtype=bool), "Stain1"),
+        Channel(da.ones((5,5), dtype=bool), "Stain2"),
+        Channel(da.ones((5,5), dtype=bool), "Stain3"),
+        Channel(da.ones((5,5), dtype=bool), "Stain4"),
+        Channel(da.ones((5,5), dtype=bool), "Stain5")
+    ]
+    channel_set = BinaryMaskChannelSet(channels)
+    assert channel_set.channels == channels
+    assert channel_set.shape == (5,5)
+    assert channel_set.dtype == bool
