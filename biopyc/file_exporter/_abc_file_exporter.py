9c2dc9328229bd5ecc9793139cc1b58e700e8722
Hugh Warden <hugh.warden@outlook.com>
2025-04-08 12:33:07 +0100

    Linting file exporter

--- biopyc/file_exporter/_abc_file_exporter.py
+++ biopyc/file_exporter/_abc_file_exporter.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 from dask.dataframe import DataFrame
 
 
-class _ABCFileExporter:
+class _ABCFileExporter(ABC):
     @abstractmethod
     def export(self, df: DataFrame, path: str) -> None:
         pass
