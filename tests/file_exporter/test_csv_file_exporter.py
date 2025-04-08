afa0af62b763119cfe5a712318e085d168f995d5
Hugh Warden <hugh.warden@outlook.com>
2025-04-08 13:04:54 +0100

    add tests

---
+++ tests/file_exporter/test_csv_file_exporter.py
@@ -0,0 +1,21 @@
+from biopyc.file_exporter._csv_file_exporter import CSVFileExporter
+import dask.dataframe as dd
+import os
+from pandas import DataFrame
+from pytest import raises
+from tempfile import TemporaryDirectory
+
+def test_csv_file_exporter_validation():
+    pddf = DataFrame({"Column1": [1, 2, 3], "Column2": [4, 5, 6]})
+    df = dd.from_pandas(pddf)
+    temp_dir = TemporaryDirectory()
+    csv_file_path = os.path.join(str(temp_dir), "test_csv.csv")
+    exising_dir = "./tests"
+    with raises(TypeError, match="Supplied data is not a dask data frame"):
+        CSVFileExporter().export(1, csv_file_path)
+    with raises(TypeError, match="Supplied file path is not a string"):
+        CSVFileExporter().export(df, 1)
+    with raises(FileExistsError, match="Save file already exists"):
+        CSVFileExporter().export(df, exising_dir)
+    
+    
